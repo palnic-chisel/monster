569789283ac3cf1094521dbc77820079d65f00da
palnic-chisel <nicola.palumbo1993@gmail.com>
2020-01-17 15:31:22 +0100

    Updates

---
+++ precache-manifest.63f6ae66299905a87716c93159c2ed9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a893a136dc8e32fb77a9854df4b9d061",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "6b25e28923def48d420a",
+    "url": "/monster/static/css/main.b2bfb140.chunk.css"
+  },
+  {
+    "revision": "b6b4b7653795c37734cb",
+    "url": "/monster/static/js/2.81bb5c18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster/static/js/2.81bb5c18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b25e28923def48d420a",
+    "url": "/monster/static/js/main.ffcb578b.chunk.js"
+  },
+  {
+    "revision": "30c5ee8600922fc3d567",
+    "url": "/monster/static/js/runtime-main.55c9f17f.js"
+  }
+]);
